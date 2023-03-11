17e094ee27c452294bc998f0093b00207afdfcd2
Ratneesh <ratneesh_kv@mt.iitr.ac.in>
2023-03-11 23:14:58 +0530

    second commit

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1,2 +1,2 @@
-export const WHITELIST_CONTRACT_ADDRESS = "0xC6F899872ac0d307f973E24A229827fb9050d9ce";
+export const WHITELIST_CONTRACT_ADDRESS = "0x08cdD39d6becfBa890ea4D98A658D687e47E5d22";
 export const abi = "hardhat-tutorial/artifacts/contracts/Whitelist.sol";
